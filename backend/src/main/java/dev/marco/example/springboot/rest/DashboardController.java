ed64eb1445dfa3c614ac6478d1a21543a3659515
Danigri-source <dankagrigorev@gmail.com>
2021-08-02 10:00:37 +0300

    Added DashboardController

--- backend/src/main/java/dev/marco/example/springboot/rest/DashboardController.java
+++ backend/src/main/java/dev/marco/example/springboot/rest/DashboardController.java
@@ -1,5 +1,32 @@
 package dev.marco.example.springboot.rest;
 
+import dev.marco.example.springboot.exception.*;
+import dev.marco.example.springboot.model.User;
+import dev.marco.example.springboot.service.DashboardService;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
 public class DashboardController {
 
+    @Autowired
+    private DashboardService dashboardService;
+
+    private static final Logger log = Logger.getLogger(UserController.class);
+
+    public void generateDashboard(@RequestBody User user) {
+        try {
+            dashboardService.generateDashboard(user);
+        } catch (DAOLogicException e) {
+            log.error(MessagesForException.DAO_LOGIC_EXCEPTION);
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), e.getCause());
+        } catch (QuizDoesNotExistException | AnnouncementDoesNotExistException | AnnouncementException e) {
+            log.error(MessagesForException.MESSAGE_ERROR);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e.getCause());
+        }
+    }
 }
