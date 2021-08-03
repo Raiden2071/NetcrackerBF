94cef9a10387137a5c13be2b5b281a87779c03ed
Maksym Bataiev <max.bataiev@gmail.com>
2021-08-04 01:53:43 +0300

    confirm email small refactor

--- backend/src/main/java/dev/marco/example/springboot/rest/UserController.java
+++ backend/src/main/java/dev/marco/example/springboot/rest/UserController.java
@@ -23,198 +23,213 @@ import static dev.marco.example.springboot.exception.MessagesForException.*;
 @RestController
 public class UserController implements RegexPatterns {
 
-    private static final Logger log = Logger.getLogger(UserController.class);
+  private static final Logger log = Logger.getLogger(UserController.class);
 
-    @Autowired
-    private UserService userService;
+  @Autowired
+  private UserService userService;
 
-    @Autowired
-    private MailSenderService mailSenderService;
+  @Autowired
+  private MailSenderService mailSenderService;
 
-    @Autowired
-    public void setTestConnection() throws DAOConfigException {
-        userService.setTestConnection();
-        mailSenderService.setTestConnection();
+  @Autowired
+  public void setTestConnection() throws DAOConfigException {
+    userService.setTestConnection();
+    mailSenderService.setTestConnection();
+  }
+
+  @PostMapping("/auth/local/register")
+  public ResponseEntity<Object> createUser(@RequestBody UserImpl user) {
+    log.info("mail: " + user.getEmail() + "firstName: " + user.getFirstName());
+    try {
+      userService.validateNewUser(
+          user.getEmail(),
+          user.getPassword(),
+          user.getFirstName(),
+          user.getLastName());
+      BigInteger userId = userService.buildNewUser(
+          user.getEmail(),
+          user.getPassword(),
+          user.getFirstName(),
+          user.getLastName());
+
+      User user1 = new UserImpl.UserBuilder()
+          .setId(userId)
+          .setEmail(user.getEmail())
+          .build();
+
+      if (!mailSenderService.sendEmail(user1)) {
+        throw new MailException(EMAIL_ERROR);
+      }
+      return ResponseEntity.ok().build();
+
+    } catch (DAOLogicException | MailException | UserException e) {
+      log.error(e.getMessage());
+      return ResponseEntity.badRequest().build();
     }
+  }
 
-    @PostMapping("/auth/local/register")
-    public void createUser(@RequestBody UserImpl user) {
-        log.info("mail: " + user.getEmail() + "firstName: " + user.getFirstName());
-        try {
-            userService.validateNewUser(
-                    user.getEmail(),
-                    user.getPassword(),
-                    user.getFirstName(),
-                    user.getLastName());
-            BigInteger userId = userService.buildNewUser(
-                    user.getEmail(),
-                    user.getPassword(),
-                    user.getFirstName(),
-                    user.getLastName());
+  @PostMapping("/auth/local")
+  public ResponseEntity<User> tryToAuthorize(@RequestBody UserImpl user) {
+    try {
+      if (!user.getEmail().matches(mailPattern)) {
+        throw new UserException(MessagesForException.INVALID_EMAIL);
+      }
+      if (!user.getPassword().matches(passPattern)) {
+        throw new UserException(MessagesForException.INVALID_PASSWORD);
+      }
+      User receivedUser = userService.authorize(user);
 
-            User user1 = new UserImpl.UserBuilder()
-                    .setId(userId)
-                    .setEmail(user.getEmail())
-                    .build();
-
-            mailSenderService.sendEmail(user1);
-        } catch (DAOLogicException | MailException | UserException e) {
-            log.error(e.getMessage());
-        }
+      return ResponseEntity.ok(receivedUser);
+    } catch (DAOLogicException | UserException | UserDoesNotExistException e) {
+      log.error(e.getMessage());
+      return ResponseEntity.badRequest().build();
     }
+  }
 
-    @PostMapping("/auth/local")
-    public ResponseEntity<User> tryToAuthorize(@RequestBody UserImpl user) {
-        try {
-            if (!user.getEmail().matches(mailPattern)) {
-                throw new UserException(MessagesForException.INVALID_EMAIL);
-            }
-            if (!user.getPassword().matches(passPattern)) {
-                throw new UserException(MessagesForException.INVALID_PASSWORD);
-            }
-            User receivedUser = userService.authorize(user);
-
-            return ResponseEntity.ok(receivedUser);
-        } catch (DAOLogicException | UserException | UserDoesNotExistException e) {
-            log.error(e.getMessage());
-            return ResponseEntity.notFound().build();
-        }
+  @PostMapping("/recover")
+  public ResponseEntity<Object> recoverPassword(@RequestBody String email) {
+    try {
+      User user = new UserImpl.UserBuilder()
+          .setEmail(email)
+          .build();
+      if (!userService.recoverPassword(user)) {
+        log.error(MessagesForException.EMAIL_ERROR);
+        throw new MailException(MessagesForException.EMAIL_ERROR);
+      }
+      return ResponseEntity.ok().build();
+    } catch (DAOLogicException | MailException | UserException e) {
+      log.error("Error while recoverPassword() with email=" + email + " " + e.getMessage());
+      return ResponseEntity.badRequest().build();
     }
+  }
 
-    @PostMapping("/recover")
-    public void recoverPassword(@RequestBody String email) {
-        try {
-            User user = new UserImpl.UserBuilder()
-                    .setEmail(email)
-                    .build();
-            if (!userService.recoverPassword(user)) {
-                log.error(MessagesForException.EMAIL_ERROR);
-                throw new MailException(MessagesForException.EMAIL_ERROR);
-            }
-        } catch (DAOLogicException | MailException | UserException e) {
-            log.error("Error while recoverPassword() with email=" + email + " " + e.getMessage());
-        }
+  @PutMapping("/updatePassword")
+  public void updatePassword(BigInteger id, String newPassword) throws UserException {
+    try {
+      if (id == null) {
+        throw new UserDoesNotExistException(MessagesForException.USER_NOT_FOUND_EXCEPTION);
+      }
+      if (!newPassword.matches(passPattern)) {
+        throw new UserException(MessagesForException.INVALID_PASSWORD);
+      }
+      userService.updateUsersPassword(id, newPassword);
+    } catch (DAOLogicException | UserDoesNotExistException e) {
+      e.printStackTrace();
     }
+  }
 
-    @PutMapping("/updatePassword")
-    public void updatePassword(BigInteger id, String newPassword) throws UserException {
-        try {
-            if (id == null) {
-                throw new UserDoesNotExistException(MessagesForException.USER_NOT_FOUND_EXCEPTION);
-            }
-            if (!newPassword.matches(passPattern)) {
-                throw new UserException(MessagesForException.INVALID_PASSWORD);
-            }
-            userService.updateUsersPassword(id, newPassword);
-        } catch (DAOLogicException | UserDoesNotExistException e) {
-            e.printStackTrace();
-        }
+  public void registrationConfirm(User user) {
+    //free
+
+  }
+
+  @PostMapping("/confirm")
+  public ResponseEntity<User> confirmEmail(@RequestParam String code) {
+    try {
+      if (StringUtils.isEmpty(code)) {
+        throw new MailException(MessagesForException.EMAIL_ERROR);
+      }
+
+      User user = mailSenderService.confirmEmail(code);
+
+      if(user == null)
+        throw new MailException(EMAIL_ERROR);
+      return ResponseEntity.ok(user);
+    } catch (DAOLogicException | MailException | UserException e) {
+      log.error(EMAIL_ERROR);
+      return ResponseEntity.badRequest().build();
     }
+  }
 
-    public void registrationConfirm(User user) {
-        //free
-
+  @GetMapping("/user")
+  public User getUser(@RequestBody BigInteger idUser) {
+    try {
+      return userService.getUserById(idUser);
+    } catch (UserDoesNotExistException e) {
+      log.error(e.getMessage(), e);
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e.getCause());
+    } catch (DAOLogicException e) {
+      log.error(e.getMessage(), e);
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(),
+          e.getCause());
     }
+  }
 
-    @PostMapping("/confirm")
-    public void confirmEmail(@RequestParam String code) {
-        try {
-            if (StringUtils.isEmpty(code)) {
-                throw new MailException(MessagesForException.EMAIL_ERROR);
-            }
-            mailSenderService.confirmEmail(code);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+  @DeleteMapping("/user")
+  public void deleteUser(@RequestBody BigInteger idUser) {
+    try {
+      userService.deleteUser(idUser);
+    } catch (DAOLogicException e) {
+      log.error(e.getMessage(), e);
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(),
+          e.getCause());
+    } catch (UserDoesNotExistException e) {
+      log.error(e.getMessage(), e);
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e.getCause());
     }
+  }
 
-    @GetMapping("/user")
-    public User getUser(@RequestBody BigInteger idUser) {
-        try {
-            return userService.getUserById(idUser);
-        } catch (UserDoesNotExistException e) {
-            log.error(e.getMessage(), e);
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e.getCause());
-        } catch (DAOLogicException e) {
-            log.error(e.getMessage(), e);
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(),
-                    e.getCause());
-        }
+  @PutMapping("/user")
+  public void editUser(@RequestBody UserImpl user) {
+    try {
+      if (StringUtils.isBlank(user.getFirstName()) || StringUtils.isBlank(user.getLastName())) {
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+      } else {
+        userService.updateUsersFullName(user.getId(), user.getFirstName(), user.getLastName());
+      }
+
+      if (StringUtils.isBlank(user.getDescription())) {
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+      } else {
+        userService.updateUsersDescription(user.getId(), user.getDescription());
+      }
+
+      if (StringUtils.isBlank(user.getPassword())) {
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+      } else {
+        userService.updateUsersPassword(user.getId(), user.getPassword());
+      }
+    } catch (DAOLogicException e) {
+      log.error(e.getMessage(), e);
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(),
+          e.getCause());
+    } catch (UserDoesNotExistException e) {
+      log.error(e.getMessage(), e);
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e.getCause());
     }
+  }
 
-    @DeleteMapping("/user")
-    public void deleteUser(@RequestBody BigInteger idUser) {
-        try {
-            userService.deleteUser(idUser);
-        } catch (DAOLogicException e) {
-            log.error(e.getMessage(), e);
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(),
-                    e.getCause());
-        } catch (UserDoesNotExistException e) {
-            log.error(e.getMessage(), e);
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e.getCause());
-        }
+  @GetMapping("/user/acc_quiz")
+  public Set<QuizAccomplishedImpl> getAccomplishedQuizzes(@RequestParam BigInteger userId) {
+    try {
+      return userService.getAccomplishedQuizesByUser(userId);
+    } catch (DAOLogicException e) {
+      log.error(DAO_LOGIC_EXCEPTION + e.getMessage());
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(),
+          e.getCause());
+    } catch (QuizDoesNotExistException e) {
+      log.error(QUIZ_NOT_FOUND_EXCEPTION + e.getMessage());
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e.getCause());
+    } catch (UserDoesNotExistException e) {
+      log.error(USER_NOT_FOUND_EXCEPTION + e.getMessage());
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e.getCause());
     }
+  }
 
-    @PutMapping("/user")
-    public void editUser(@RequestBody UserImpl user) {
-        try {
-            if (StringUtils.isBlank(user.getFirstName()) || StringUtils.isBlank(user.getLastName())) {
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-            } else {
-                userService.updateUsersFullName(user.getId(), user.getFirstName(), user.getLastName());
-            }
-
-            if (StringUtils.isBlank(user.getDescription())) {
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-            } else {
-                userService.updateUsersDescription(user.getId(), user.getDescription());
-            }
-
-            if (StringUtils.isBlank(user.getPassword())) {
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-            } else {
-                userService.updateUsersPassword(user.getId(), user.getPassword());
-            }
-        } catch (DAOLogicException e) {
-            log.error(e.getMessage(), e);
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(),
-                    e.getCause());
-        } catch (UserDoesNotExistException e) {
-            log.error(e.getMessage(), e);
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e.getCause());
-        }
+  @GetMapping("/user/favorite")
+  public Set<QuizAccomplishedImpl> getFavoriteQuizesByUser(@RequestBody UserImpl user) {
+    try {
+      return userService.getFavoriteQuizesByUser(user.getId());
+    } catch (DAOLogicException e) {
+      log.error(DAO_LOGIC_EXCEPTION + e.getMessage());
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(),
+          e.getCause());
+    } catch (QuizDoesNotExistException e) {
+      log.error(QUIZ_NOT_FOUND_EXCEPTION + e.getMessage());
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e.getCause());
+    } catch (UserDoesNotExistException e) {
+      log.error(USER_NOT_FOUND_EXCEPTION + e.getMessage());
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e.getCause());
     }
-
-    @GetMapping("/user/acc_quiz")
-    public Set<QuizAccomplishedImpl> getAccomplishedQuizzes(@RequestParam BigInteger userId) {
-        try {
-            return userService.getAccomplishedQuizesByUser(userId);
-        } catch (DAOLogicException e) {
-            log.error(DAO_LOGIC_EXCEPTION + e.getMessage());
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), e.getCause());
-        } catch (QuizDoesNotExistException e) {
-            log.error(QUIZ_NOT_FOUND_EXCEPTION + e.getMessage());
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e.getCause());
-        } catch (UserDoesNotExistException e) {
-            log.error(USER_NOT_FOUND_EXCEPTION + e.getMessage());
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e.getCause());
-        }
-    }
-
-    @GetMapping("/user/favorite")
-    public Set<QuizAccomplishedImpl> getFavoriteQuizesByUser(@RequestBody UserImpl user) {
-        try {
-            return userService.getFavoriteQuizesByUser(user.getId());
-        } catch (DAOLogicException e) {
-            log.error(DAO_LOGIC_EXCEPTION + e.getMessage());
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), e.getCause());
-        } catch (QuizDoesNotExistException e) {
-            log.error(QUIZ_NOT_FOUND_EXCEPTION + e.getMessage());
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e.getCause());
-        } catch (UserDoesNotExistException e) {
-            log.error(USER_NOT_FOUND_EXCEPTION + e.getMessage());
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e.getCause());
-        }
-    }
+  }
 }
