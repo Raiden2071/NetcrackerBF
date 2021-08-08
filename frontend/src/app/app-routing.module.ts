a021df2a2af0147ec702d7d262883faec0532803
Maksym Bataiev <max.bataiev@gmail.com>
2021-08-08 15:48:44 +0300

    jopa

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,8 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 import { AuthGuard } from './modules/auth/auth.guard';
 import { QuizQuestionsComponent } from './modules/quiz/components/quiz-questions/quiz-questions.component';
-
-
+import { QuizGuard } from './modules/quiz/quiz.guard';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/auth/login' },
@@ -17,14 +16,15 @@ const routes: Routes = [
       { path: 'profile', loadChildren: () => import('./modules/profile/profile/profile.module').then(m => m.ProfileModule) },
       { path: 'test', loadChildren: () => import('./modules/test/test.module').then(m => m.TestModule) },
       { path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule) },
-      { path: 'announcment', loadChildren: () => import('./modules/announcement/announcment.module').then(m => m.AnnouncmentModule) },
+      { path: 'announcement', loadChildren: () => import('./modules/announcement/announcment.module').then(m => m.AnnouncmentModule) },
       { path: 'settings', loadChildren: () => import('./modules/settings/settings.module').then(m => m.SettingsModule) },
       { path: 'quiz', loadChildren: () => import('./modules/quiz/quiz.module').then(m => m.QuizModule) },
-      // sorry
       { path: 'quiz/quiz-questions', component: QuizQuestionsComponent },
-
     ],
     // canActivate: [AuthGuard]
+  },
+  {
+    path: 'quiz/quiz-questions', component: QuizQuestionsComponent, canActivate: [QuizGuard]
   },
   {
     path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
--- frontend/src/app/shared/services/users.service.ts
+++ frontend/src/app/shared/services/users.service.ts
@@ -29,8 +29,8 @@ export class UserService extends AbstractCrudService<User>{
   }
 
   getMe(): Observable<User> {
-    // ${this.userId}
-    return this.http.get<User>(`user/182`);
+    this.userId = 46;
+    return this.http.get<User>(`user/46`);
   }
 
 }
