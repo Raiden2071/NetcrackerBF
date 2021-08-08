713b55d4761ddd36725394d8709b20be6fb3fd58
Raiden2071 <66531600+Raiden2071@users.noreply.github.com>
2021-08-08 18:07:05 +0300

    ропро

---
+++ frontend/src/app/modules/quiz/quiz.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '../auth/services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuizGuard implements CanActivate {
+
+  data = false;
+
+  constructor(private authService: AuthService, private router: Router) {}
+  
+  canActivate(): boolean {
+    if(!this.authService.isAuthenticated) {
+      this.router.navigateByUrl('/auth/login');
+      return false;
+    }
+    if (!this.data) {
+      this.router.navigateByUrl('/quiz');
+      return false;
+    }
+    this.router.navigateByUrl('/quiz/quiz-questions');
+    return true;
+  }
+
+  
+}
