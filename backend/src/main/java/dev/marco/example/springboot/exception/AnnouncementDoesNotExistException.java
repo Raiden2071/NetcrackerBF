cfa2cc3148b98de1efa47d6e746d4b809f55d807
Maksym Bataiev <max.bataiev@gmail.com>
2021-08-13 04:17:01 +0300

    exceptions fix

--- backend/src/main/java/dev/marco/example/springboot/exception/AnnouncementDoesNotExistException.java
+++ backend/src/main/java/dev/marco/example/springboot/exception/AnnouncementDoesNotExistException.java
@@ -2,11 +2,11 @@ package dev.marco.example.springboot.exception;
 
 public class AnnouncementDoesNotExistException extends Exception implements MessagesForException {
 
-    public AnnouncementDoesNotExistException(String errorMessage, Throwable error){
-        super(errorMessage, error);
-    }
+  public AnnouncementDoesNotExistException(String errorMessage, Throwable error) {
+    super(errorMessage, error);
+  }
 
-    public AnnouncementDoesNotExistException(String errorMessage){
-        super(errorMessage);
-    }
+  public AnnouncementDoesNotExistException(String errorMessage) {
+    super(errorMessage);
+  }
 }
--- backend/src/main/java/dev/marco/example/springboot/exception/QuestionDoesNotExistException.java
+++ backend/src/main/java/dev/marco/example/springboot/exception/QuestionDoesNotExistException.java
@@ -2,16 +2,11 @@ package dev.marco.example.springboot.exception;
 
 public class QuestionDoesNotExistException extends Exception {
 
-    public QuestionDoesNotExistException(String errorMessage) {
-        super(errorMessage);
-    }
+  public QuestionDoesNotExistException(String errorMessage) {
+    super(errorMessage);
+  }
 
-    public QuestionDoesNotExistException(String errorMessage, Throwable error) {
-        super(errorMessage, error);
-    }
-
-    @Override
-    public String toString() {
-        return "Question not found";
-    }
+  public QuestionDoesNotExistException(String errorMessage, Throwable error) {
+    super(errorMessage, error);
+  }
 }
